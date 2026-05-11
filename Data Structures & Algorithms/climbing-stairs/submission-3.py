af7ec09e2496158c180b5640bacede0b3238b577
Tyson Franke <126979780+tfranke0814@users.noreply.github.com>
2026-05-11 10:31:26 -0500

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,12 @@
+import math
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+
+        prev1, prev2 = 1, 2
+        for i in range(3, n+1):
+            curr = prev1 + prev2
+            prev1 = prev2
+            prev2 = curr
+        return prev2
